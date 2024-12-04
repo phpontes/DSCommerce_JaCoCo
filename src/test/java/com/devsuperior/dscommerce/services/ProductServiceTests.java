2fc999244a49a2169eec5d9863283e7d876f8aa8
phpontes <ppontes95@hotmail.com>
2024-12-04 07:58:42 -0300

    Insert new product

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -37,6 +37,7 @@ public class ProductServiceTests {
 	private long existingProductId, nonExistingProductId;
 	private String productName;
 	private Product product;
+	private ProductDTO productDTO;
 	private PageImpl<Product> page;
 	
 	@BeforeEach
@@ -47,12 +48,15 @@ public class ProductServiceTests {
 		productName = "Preisteishon 5";
 		
 		product = ProductFactory.createProduct(productName);
+		productDTO = new ProductDTO(product);
 		page = new PageImpl<>(List.of(product));
 		
 		Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
 		
 		Mockito.when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);
+		
+		Mockito.when(repository.save(any())).thenReturn(product);
 	}
 	
 	@Test
@@ -82,5 +86,14 @@ public class ProductServiceTests {
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getSize(), 1);
 		Assertions.assertEquals(result.iterator().next().getName(), productName);
+	}
+	
+	@Test
+	public void insertShouldReturnProductDTO() {
+		
+		ProductDTO result = service.insert(productDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), product.getId());
 	}
 }
